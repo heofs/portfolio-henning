0122996c50abcf467de449fa31f138013519ff3c
Henning Ofstad <ofstad10@gmail.com>
2020-05-13 15:23:04 +0200

    Add personal logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         background_color: `#303030`,
         theme_color: `#303030`,
         display: `minimal-ui`,
-        icon: `src/images/favicon-js.svg`, // This path is relative to the root of the site.
+        icon: `src/images/logo-cloud.svg`, // This path is relative to the root of the site.
       },
     },
     {
